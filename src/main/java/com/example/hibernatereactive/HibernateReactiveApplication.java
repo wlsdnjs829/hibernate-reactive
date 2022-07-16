30a973684f1356d1963c2ed3c0ddc980d20d64fe
ljw0829 <olo_jinwon@naver.com>
2022-07-16 16:22:33 +0900

    - feat : hibernate-reactive 연습 프로젝트

---
+++ src/main/java/com/example/hibernatereactive/HibernateReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernatereactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateReactiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hibernatereactive/HibernateReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibernatereactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
